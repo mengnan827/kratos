e9859d2cabc4327bb10e192dd71e2825370da8be
“luojunying” <727366586@qq.com>
2026-01-17 14:21:13 +0800

    chore

--- api/metadata/server.go
+++ api/metadata/server.go
@@ -1,3 +1,29 @@
 package metadata
 
-type Server struct{}
+import (
+	"sync"
+
+	"google.golang.org/grpc"
+	dpb "google.golang.org/protobuf/types/descriptorpb"
+)
+
+type Server struct {
+	UnimplementedMetadataServer
+
+	lock     sync.Mutex
+	srv      *grpc.Server
+	services map[string]*dpb.FileDescriptorSet
+	methods  map[string][]string
+}
+
+// NewServer 创建一个元数据服务
+func NewServer(srv *grpc.Server) *Server {
+	return &Server{
+		srv:      srv,
+		services: make(map[string]*dpb.FileDescriptorSet),
+		methods:  make(map[string][]string),
+	}
+}
+
+func (s *Server) load() error {
+}
